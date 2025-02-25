b327777b71adb44200377691283e1f56f84ac017
ocolunga <oscar@ancile.dev>
2025-02-24 23:17:42 -0600

    Fix Nix-Darwin move

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Dev Mac nix-darwin system flake";
+  description = "NixOS and Nix-Darwin configuration for all my systems";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -15,117 +15,12 @@
       nixpkgs,
       nix-homebrew,
     }:
-    let
-      configuration =
-        { pkgs, config, ... }:
-        {
-          # List packages installed in system profile. To search by name, run:
-          # $ nix-env -qaP | grep wget
-
-          nixpkgs.config.allowUnfree = true;
-
-          environment.systemPackages = [
-            pkgs.vim
-            pkgs.neovim
-            pkgs.mkalias
-            pkgs.nixfmt-rfc-style
-            pkgs.uv
-          ];
-
-          homebrew = {
-            enable = true;
-            taps = [
-              "nikitabobko/tap"
-              "FelixKratz/formulae"
-            ];
-            brews = [
-              "mas"
-              "borders"
-              "mactop"
-            ];
-            casks = [
-              "microsoft-edge"
-              "visual-studio-code"
-              "aerospace"
-              "ghostty"
-              "obsidian"
-              "zoom"
-            ];
-            masApps = {
-              "Microsoft Word" = 462054704;
-              "Microsoft Excel" = 462058435;
-              "Microsoft PowerPoint" = 462062816;
-              "Microsoft Outlook" = 985367838;
-              #"Microsoft OneDrive" = 823766827;
-              "Pasty" = 1544620654;
-              "WhatsApp" = 310633997;
-              "Slack" = 803453959;
-              "Bitwarden" = 1352778147;
-              "WireGuard" = 1451685025;
-            };
-            onActivation.cleanup = "zap";
-            onActivation.autoUpdate = true;
-            onActivation.upgrade = true;
-          };
-
-          fonts.packages = [
-            pkgs.monaspace
-          ];
-
-          system.activationScripts.applications.text =
-            let
-              env = pkgs.buildEnv {
-                name = "system-applications";
-                paths = config.environment.systemPackages;
-                pathsToLink = "/Applications";
-              };
-            in
-            pkgs.lib.mkForce ''
-              # Set up applications.
-                  echo "setting up /Applications..." >&2
-                  rm -rf /Applications/Nix\ Apps
-                  mkdir -p /Applications/Nix\ Apps
-                  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-                  while read -r src; do
-                    app_name=$(basename "$src")
-                    echo "copying $src" >&2
-                    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-                  done
-            '';
-
-          # Add Rosetta installation script
-          system.activationScripts.extraActivation.text = ''
-            if ! /usr/bin/pgrep oahd >/dev/null 2>&1; then
-              softwareupdate --install-rosetta --agree-to-license
-            fi
-          '';
-
-          # Enable Touch ID for sudo authentication
-          security.pam.services.sudo_local.touchIdAuth = true;
-
-          # Necessary for using flakes on this system.
-          nix.settings.experimental-features = "nix-command flakes";
-
-          # Enable alternative shell support in nix-darwin.
-          # programs.fish.enable = true;
-
-          # Set Git commit hash for darwin-version.
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-
-          # Used for backwards compatibility, please read the changelog before changing.
-          # $ darwin-rebuild changelog
-          system.stateVersion = 6;
-
-          # The platform the configuration will be used on.
-          nixpkgs.hostPlatform = "aarch64-darwin";
-        };
-    in
     {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#mac-dev
       darwinConfigurations."mac-dev" = nix-darwin.lib.darwinSystem {
         modules = [
-          configuration
+          ./nix-darwin/dev.nix
           nix-homebrew.darwinModules.nix-homebrew
           {
             nix-homebrew = {
